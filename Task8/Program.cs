23ab678b9b68c15a8191e4bb194547ffbd0e8e62
iryna <rinakinsha@yndex.by>
2022-06-20 22:48:36 +0300

    Solution of task No. 8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int n = 1;
+
+while (n<=N)
+{
+    if(n%2==0)
+    {
+        Console.Write(n + " ");
+    }
+n++;    
+}
