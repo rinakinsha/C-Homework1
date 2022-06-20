a75c97caec2e67720fe932972eeba0eb9874eb6b
iryna <rinakinsha@yndex.by>
2022-06-20 22:03:24 +0300

    Solution of task No. 6

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if(a%2==0)
+{
+    Console.WriteLine(a + " - четное число");
+}
+    else
+{
+    Console.WriteLine(a + " - нечетное число");
+}
