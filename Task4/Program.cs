ede00caf98df2ba651a04e2dabbd6e6713008b0b
iryna <rinakinsha@yndex.by>
2022-06-20 21:35:40 +0300

    Solution of task No. 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿double a, b, c;
+
+Console.Write("Введите 1 число: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 3 число: ");
+c = Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+
+if(b>max)
+{
+    max = b;
+}
+if(c>max)
+{
+    max = c;
+}
+
+Console.WriteLine("Максимальное число: "+ max);
