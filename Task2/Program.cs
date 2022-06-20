afd233c7e8f728e35d8fb858f85da573709739b6
iryna <rinakinsha@yndex.by>
2022-06-20 20:41:21 +0300

    Solution of task No. 2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿double a, b;
+Console.Write("Введите 1 число: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+b = Convert.ToDouble(Console.ReadLine());
+
+
+if (a>b)
+{
+    Console.WriteLine("max = "+ a);
+    Console.WriteLine("min = "+ b);
+}
+if (a<b)
+{
+    Console.WriteLine("max = "+ b);
+    Console.WriteLine("min = "+ a);
+}
+if (a==b)
+{
+    Console.WriteLine("Числа равны");
+}
